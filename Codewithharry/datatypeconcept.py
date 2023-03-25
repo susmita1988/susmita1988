c417304a08bcfdbdcd41f14b79d76bc3251224d7
Susmita <susmita88.mca@gmail.com>
2023-03-25 12:11:43 +0530

    saving python file in git

---
+++ Codewithharry/datatypeconcept.py
@@ -0,0 +1,21 @@
+var1="Hello world"
+var2="4"
+var3="36.7"
+
+#print(type(var3))
+
+#var2=int(var2)
+#print(var2)
+
+#to print in new lines
+print(10*" mampi\n")
+
+
+#take input from user
+print("enter your number:")
+inpnum=input()
+print(int(inpnum)+10)
+
+
+
+
